56205044b644acfdedb274a6dde56ff9dc0b1b66
renard314 <renard.wellnitz@googlemail.com>
2017-09-03 20:12:05 +0200

    test: setup annotation processor tests

---
+++ auto-adapter-processor/src/test/java/com/renard/auto_adapter/processor/AutoAdapterProcessorTest.java
@@ -0,0 +1,21 @@
+package com.renard.auto_adapter.processor;
+
+public class AutoAdapterProcessorTest {
+
+    public void test() {
+// ASSERT.about(javaSource()).that()
+////        ASSERT.about(javaSource())
+////                .that(JavaFileObjects.forResource("com/visma/test/apt/unittest/ClassWithoutIgnore.java"))
+////                .processedWith(new IgnoreAnnotationProcessor())
+////                .compilesWithoutError();
+// JavaSourceSubjectFactory javaSourceSubjectFactory = javaSource();
+//
+// CompilationRule compilationRule = javac()
+// /*    Compilation compilation =
+// *          javac()
+// *                  .withProcessors(new MyAnnotationProcessor())
+// *                  .compile(JavaFileObjects.forSourceString("HelloWorld", "final class HelloWorld {}");
+// *  assertThat(compilation).succeededWithoutWarnings();*/
+    }
+
+}
